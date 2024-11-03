e32137b5b7f866a708e355c0c42e68d95eca92e0
rick-savior <ctm_rd@hotmail.com>
2024-11-03 18:47:57 -0300

    Versão Final
    Projeto feito para gravação do video.

--- Cafeteria/Cafeteria/a1/static/js/script.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('botao').onclick = function() {
-    document.getElementById('mensagem').innerHTML = 'Você clicou no botão!';
-};
--- Cafeteria/Cafeteria/a1/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def cafe(request):
-    return render(request, 'a1/index.html', {})
